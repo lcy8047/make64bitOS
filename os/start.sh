bad3945b257e34a9d5324c957e79ef6cbcb36f82
wjsdurtlzuwnj <wjsdurtlzuwnj0701@gmail.com>
2021-07-08 16:38:34 +0900

    fix disk read error - no use raw option

--- os/start.sh
+++ os/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DISK="./Disk.img"
 QEMU=qemu-system-x86_64
-$QEMU -L . -m 64 -drive file=$DISK,format=raw -M pc -curses
+#$QEMU -L . -m 64 -drive file=$DISK,format=raw -M pc -curses
+$QEMU -m 64 -fda $DISK -M pc -curses
