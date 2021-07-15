f443945a18edb57232be812bfc9db233f967f3c0
lcy8047 <lcy8047@gmail.com>
2021-07-15 18:23:02 +0900

    ch10 - modify for IA-32e mode

--- os/01.Kernel32/source/EntryPoint.s
+++ os/01.Kernel32/source/EntryPoint.s
@@ -37,14 +37,14 @@ START:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     mov eax, 0x4000003B ; PG=0, CD=1, NW=0, AM=0, WP=0, NE=1, ET=1, TS=1, EM=0, MP=1, PE=1
     mov cr0, eax
-    jmp dword 0x08: ( PROTECTEDMODE - $$ + 0x10000 )
+    jmp dword 0x18: ( PROTECTEDMODE - $$ + 0x10000 )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; entry protected mode
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 [BITS 32]
 PROTECTEDMODE:
-    mov ax, 0x10
+    mov ax, 0x20
     mov ds, ax
     mov es, ax
     mov fs, ax
@@ -62,7 +62,7 @@ PROTECTEDMODE:
     call PRINTMESSAGE
     add esp, 12
     
-    jmp dword 0x08: 0x10200 ; jmp to C language kernel
+    jmp dword 0x18: 0x10200 ; jmp to C language kernel
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ; functions
@@ -139,6 +139,22 @@ GDT:
         db 0x00
         db 0x00
         db 0x00
+
+    IA_32eCODEDESCRIPTOR:
+        dw 0xFFFF   ; Limit [15:0]
+        dw 0x0000   ; Base [15:0]
+        db 0x00     ; Base [23:16]
+        db 0x9A     ; P=1, DPL=0, Code Segment, Execute/Read
+        db 0xAF     ; G=1, D=0, L=1, Limit[19:16]
+        db 0x00     ; Base [31:24]
+
+    IA_32eDATADESCRIPTOR:
+        dw 0xFFFF   ; Limit [15:0]
+        dw 0x0000   ; Base [15:0]
+        db 0x00     ; Base [23:16]
+        db 0x92     ; P=1, DPL=0, Data Segment, Read/Write
+        db 0xAF     ; G=1, D=0, L=1, Limit[19:16]
+        db 0x00     ; Base [31:24]
 
     CODEDESCRIPTOR:
         dw 0xFFFF   ; Limit [15:0]
