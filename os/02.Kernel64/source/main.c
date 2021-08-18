259a5d6a79fd2d4b0da14f5874c1702ef9b3fcd8
lcy8047 <lcy8047@gmail.com>
2021-08-18 23:54:03 +0900

    ch12 - add tables init, make interrupt in shell

--- os/02.Kernel64/source/main.c
+++ os/02.Kernel64/source/main.c
@@ -1,5 +1,6 @@
 #include "types.h"
 #include "keyboard.h"
+#include "descriptor.h"
 
 // functions
 void kPrintString( int iX, int iY, const char* pcString);
@@ -14,15 +15,31 @@ void main(void)
 
     kPrintString( 0, 10, "Switch To IA-32e Mode Success" );
     kPrintString( 0, 11, "IA-32e C Language Kernel Start..............[Pass]" );
-    kPrintString( 0, 12, "Keyboard Activate...........................[    ]" );
+    
+    kPrintString( 0, 12, "GDT Initialize And Switch For IA-32e Mode...[    ]" );
+    kInitializeGDTTableAndTSS();
+    kLoadGDTR( GDTR_STARTADDRESS );
+    kPrintString( 45, 12, "Pass" );
+
+    kPrintString( 0, 13, "TSS Segment Load............................[    ]" );
+    kLoadTR( GDT_TSSSEGMENT );
+    kPrintString( 45, 13, "Pass" );
+
+    kPrintString( 0, 14, "IDT Initialize..............................[    ]" );
+    kInitializeIDTTables();
+    kLoadIDTR( IDTR_STARTADDRESS );
+    kPrintString( 45, 14, "Pass" );
+
+    kPrintString( 0, 15, "Keyboard Activate...........................[    ]" );
+
     if( kActivateKeyboard() == TRUE )
     {
-        kPrintString( 45, 12, "Pass" );
+        kPrintString( 45, 15, "Pass" );
         kChangeKeyboardLED( FALSE, FALSE, FALSE );
     }
     else
     {
-        kPrintString( 45, 12, "Fail" );
+        kPrintString( 45, 15, "Fail" );
         while(1);
     }
 
@@ -36,11 +53,19 @@ void main(void)
             {
                 if( bFlags & KEY_FLAGS_DOWN )
                 {
-                    kPrintString( i++, 13, vcTemp );
+                    kPrintString( i++, 16, vcTemp );
+
+                    // 0을 입력하면 0으로 나누는 연산을 수행해 divide error 예외 발생
+                    if( vcTemp[0] == '0')
+                    {
+                        bTemp = bTemp / 0;
+                    }
                 }
             }
         }
     }
+    
+
 }
 
 void kPrintString( int iX, int iY, const char* pcString)
